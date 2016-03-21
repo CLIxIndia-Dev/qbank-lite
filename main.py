d0243e8d931132417db466f8a0a782335a062cc3
cjshaw <cjshaw@mit.edu>
2016-03-21 14:12:53 -0400

    updated setup and main

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class index:
 def is_test():
     if 'WEBPY_ENV' in os.environ:
         return os.environ['WEBPY_ENV'] == 'test'
+    return False
 
 if (not is_test()) and __name__ == "__main__":
     serve(app.wsgifunc(), port=8091)
