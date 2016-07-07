a0fc94e10f62588c1e5b9870823b35df8dbca82d
Ubuntu <ubuntu@ip-172-31-1-91.ap-south-1.compute.internal>
2016-07-07 18:37:33 +0000

    switch to apache server

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import repository
 
 # import cherrypy
 
-from web.wsgiserver import CherryPyWSGIServer
+#from web.wsgiserver import CherryPyWSGIServer
 
 # http://pythonhosted.org/PyInstaller/runtime-information.html#run-time-information
 if getattr(sys, 'frozen', False):
@@ -19,8 +19,8 @@ else:
     PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
     ABS_PATH = '{0}/qbank-lite'.format(os.path.abspath(os.path.join(PROJECT_PATH, os.pardir)))
 
-CherryPyWSGIServer.ssl_certificate = "{0}/unplatform/unplatform.cert.dummy.pem".format(ABS_PATH)
-CherryPyWSGIServer.ssl_private_key = "{0}/unplatform/unplatform.key.dummy.pem".format(ABS_PATH)
+#CherryPyWSGIServer.ssl_certificate = "{0}/unplatform/unplatform.cert.dummy.pem".format(ABS_PATH)
+#CherryPyWSGIServer.ssl_private_key = "{0}/unplatform/unplatform.key.dummy.pem".format(ABS_PATH)
 
 web.config.debug = False
 
@@ -50,36 +50,4 @@ class video_test:
 ################################################
 # INITIALIZER
 ################################################
-
-def is_test():
-    if 'WEBPY_ENV' in os.environ:
-        return os.environ['WEBPY_ENV'] == 'test'
-    return False
-
-if (not is_test()) and __name__ == "__main__":
-    # serve(app.wsgifunc(), port=8091)
-
-    app.run()
-
-    # cherrypy.tree.mount(app)
-    #
-    # cherrypy.server.unsubscribe()
-    #
-    # server1 = cherrypy._cpserver.Server()
-    # server1.socket_port=9443
-    # server1._socket_host='0.0.0.0'
-    # server1.thread_pool=30
-    # server1.ssl_module = 'pyopenssl'
-    # server1.ssl_certificate = "{0}/unplatform/unplatform.cert.dummy.pem".format(ABS_PATH)
-    # server1.ssl_private_key = "{0}/unplatform/unplatform.key.dummy.pem".format(ABS_PATH)
-    # # server1.ssl_certificate_chain = '/home/ubuntu/gd_bundle.crt'
-    # server1.subscribe()
-    #
-    # server2 = cherrypy._cpserver.Server()
-    # server2.socket_port=9080
-    # server2._socket_host="0.0.0.0"
-    # server2.thread_pool=30
-    # server2.subscribe()
-    #
-    # cherrypy.engine.start()
-    # cherrypy.engine.block()
+application = app.wsgifunc()
