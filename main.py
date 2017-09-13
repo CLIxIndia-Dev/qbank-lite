1883c36bb4ca70da206721677278f190df024e1c
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-09-13 09:00:11 -0400

    revert main.py path change for docker...will revisit

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ from web.wsgiserver import CherryPyWSGIServer
 if getattr(sys, 'frozen', False):
     ABS_PATH = os.path.dirname(sys.executable)
 else:
-    # PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-    # ABS_PATH = '{0}/qbank-lite'.format(os.path.abspath(os.path.join(PROJECT_PATH, os.pardir)))
-    ABS_PATH = '/qbank'
+    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+    ABS_PATH = '{0}/qbank-lite'.format(os.path.abspath(os.path.join(PROJECT_PATH, os.pardir)))
 
 CherryPyWSGIServer.ssl_certificate_chain = ''
 try:
