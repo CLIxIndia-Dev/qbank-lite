4c57036f90fe3238b1730e17824e90a95b4dc496
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2018-03-22 14:18:30 -0400

    allow trailing slash in urls

--- logging_/logging_.py
+++ logging_/logging_.py
@@ -17,11 +17,11 @@ DEFAULT_LOG_GENUS_TYPE = 'log-genus-type%3Adefault-clix%40ODL.MIT.EDU'
 TEXT_BLOB_RECORD_TYPE = Type(**LOG_ENTRY_RECORD_TYPES['text-blob'])
 
 urls = (
-    "/logs", "LogsList",
-    "/logs/(.*)/logentries/(.*)", "LogEntryDetails",
-    "/logs/(.*)/logentries", "LogEntriesList",
-    "/logs/(.*)", "LogDetails",
-    "/genericlog", "GenericLogEntries"
+    "/logs/?", "LogsList",
+    "/logs/(.*)/logentries/(.*)/?", "LogEntryDetails",
+    "/logs/(.*)/logentries/?", "LogEntriesList",
+    "/logs/(.*)/?", "LogDetails",
+    "/genericlog/?", "GenericLogEntries"
 )
 
 
